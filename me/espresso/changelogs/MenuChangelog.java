d373a42bc51cd13dc6a221a9f3611d1bdf2fadfd
Espresso <71532319+UnixEspresso@users.noreply.github.com>
2022-05-20 19:07:36 -0700

    Update MenuChangelog.java

--- me/espresso/changelogs/MenuChangelog.java
+++ me/espresso/changelogs/MenuChangelog.java
@@ -2,6 +2,8 @@ package me.espresso.changelogs;
 
 import net.minecraft.client.gui.Gui;
 
+import me.espresso.ui.font.FontRenderer;
+import me.espresso.ui.font.FontManager;
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -9,28 +11,29 @@ public class MenuChangelog {
     private final ArrayList<Changelog> changelogs = new ArrayList<>();
     private final int y;
     private final int x;
-    private final CFontRenderer font = FontLoaders.Sans18;
-    public MenuChangelog(final int x, final int y){
+    private final FontRenderer font = FontManager.instance.fetch("Sans", 18);
+
+    public MenuChangelog(final int x, final int y) {
         this.x = x;
         this.y = y;
 
-        this.changelogs.add(new Changelog("Espresso 3.20", new String[]{"+ Added visuals", "+ BlocksMC Recode Flight","+ More themes."}));
-        this.changelogs.add(new Changelog("Espresso 3.19", new String[]{"+ Better visuals", "+ New Bypasses","+ Minor improvements and bug fixes."}));
+        this.changelogs.add(new Changelog("Espresso 3.20", new String[]{"+ Added visuals", "+ BlocksMC Recode Flight", "+ More themes."}));
+        this.changelogs.add(new Changelog("Espresso 3.19", new String[]{"+ Better visuals", "+ New Bypasses", "+ Minor improvements and bug fixes."}));
 
     }
 
-    public void draw(){
+    public void draw() {
         int changeY = 0;
-        Gui.drawRect(x,y,x+170, y+changelogs.size()*62, new Color(36,35,37).getRGB());
-        for(Changelog c : changelogs){
-            font.drawString(c.title(), x+10, y+10+changeY, new Color(153, 158, 255).getRGB());
+        Gui.drawRect(x, y, x + 170, y + changelogs.size() * 62, new Color(36, 35, 37).getRGB());
+        for (Changelog c : changelogs) {
+            font.drawString(c.title(), x + 10, y + 10 + changeY, new Color(153, 158, 255).getRGB());
             int set = 0;
-            for(String s : c.getUpdates()){
-                int before = y+14+changeY;
+            for (String s : c.getUpdates()) {
+                int before = y + 14 + changeY;
                 set += 12;
-                font.drawString(s,x+10, before+set,new Color(210,210,210).getRGB());
+                font.drawString(s, x + 10, before + set, new Color(210, 210, 210).getRGB());
             }
-            changeY += c.getUpdates().length*20;
+            changeY += c.getUpdates().length * 20;
         }
     }
 }
